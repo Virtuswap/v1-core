505740c2812cca64d55b8931969a995929a337f8
Daniel <daniel@virtuswap.io>
2022-07-07 10:56:35 +0300

    truffle optimizer

--- truffle-config.js
+++ truffle-config.js
@@ -120,16 +120,14 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      optimizer: {
-        enabled: false,
-        runs: 200,
-      },
-      //  evmVersion: "byzantium"
-      // }
-    },
+      version: '^0.8.0',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1500
+        }
+      }
+    }
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
