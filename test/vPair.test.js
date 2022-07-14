87430950237b7f31e27a320a2b40be9a97f94c1b
gagmirz <gmirzoyan12@gmail.com>
2022-07-14 21:27:40 +0400

    added vPair tests

--- test/vPair.test.js
+++ test/vPair.test.js
@@ -1,13 +1,18 @@
-const {sqrt } = require('bn-sqrt');
+const { sqrt } = require("bn-sqrt");
 const { toDecimalUnits, toBn } = require("./utils");
+const { solidity } = require("ethereum-waffle");
+const chai = require("chai");
 
 const vPair = artifacts.require("vPair");
 const ERC20 = artifacts.require("ERC20PresetFixedSupply");
 const vPairFactory = artifacts.require("vPairFactory");
+const vSwapMathFactory = artifacts.require("vSwapMath");
+chai.use(solidity);
+const { expect } = chai;
 
 contract("vPair", (accounts) => {
   let tokenA, tokenB;
-  let vPairFactoryInstance, vPairInstance;
+  let vPairFactoryInstance, vPairInstance, vSwapMathInstance;
   const wallet = accounts[0];
   beforeEach(async () => {
     tokenA = await ERC20.new(
@@ -32,6 +37,8 @@ contract("vPair", (accounts) => {
     );
 
     vPairInstance = await vPair.at(createdPair);
+
+    vSwapMathInstance = await vSwapMathFactory.new();
 
     await tokenA.approve(wallet, toDecimalUnits(18, 1000000));
     await tokenB.approve(wallet, toDecimalUnits(18, 1000000));
@@ -64,14 +71,12 @@ contract("vPair", (accounts) => {
   });
 
   it("Should not set whitelist if list is longer then 8", async () => {
-    let reverted = false;
-    try {
-      await vPairInstance.setWhitelist(accounts.slice(1, 10), {
+    await expect(
+      vPairInstance.setWhitelist(accounts.slice(1, 10), {
         from: accounts[2],
-      });
-    } catch (err) {
-      reverted = true;
-    }
+      })
+    ).to.revertedWith("");
+
     const response1 = await vPairInstance.whitelistAllowance(accounts[1]);
     const response2 = await vPairInstance.whitelistAllowance(accounts[2]);
     const response3 = await vPairInstance.whitelistAllowance(accounts[3]);
@@ -82,7 +87,6 @@ contract("vPair", (accounts) => {
     const response8 = await vPairInstance.whitelistAllowance(accounts[8]);
     const response9 = await vPairInstance.whitelistAllowance(accounts[9]);
 
-    expect(reverted).to.be.true;
     expect(response1).to.be.false;
     expect(response2).to.be.false;
     expect(response3).to.be.false;
@@ -95,20 +99,17 @@ contract("vPair", (accounts) => {
   });
 
   it("Should not set whitelist if not admin", async () => {
-    let reverted = false;
-    try {
-      await vPairInstance.setWhitelist(accounts.slice(1, 5), {
+    await expect(
+      vPairInstance.setWhitelist(accounts.slice(1, 5), {
         from: accounts[2],
-      });
-    } catch (err) {
-      reverted = true;
-    }
+      })
+    ).to.revertedWith("");
+
     const response1 = await vPairInstance.whitelistAllowance(accounts[1]);
     const response2 = await vPairInstance.whitelistAllowance(accounts[2]);
     const response3 = await vPairInstance.whitelistAllowance(accounts[3]);
     const response4 = await vPairInstance.whitelistAllowance(accounts[4]);
 
-    expect(reverted).to.be.true;
     expect(response1).to.be.false;
     expect(response2).to.be.false;
     expect(response3).to.be.false;
@@ -128,22 +129,18 @@ contract("vPair", (accounts) => {
   });
 
   it("Should not set factory if not admin", async () => {
-    let reverted = false;
     vPairFactoryInstance2 = await vPairFactory.new({
       from: accounts[1],
     });
 
-    try {
-      await vPairInstance.setFactory(vPairFactoryInstance2.address, {
+    await expect(
+      vPairInstance.setFactory(vPairFactoryInstance2.address, {
         from: accounts[1],
-      });
-    } catch (err) {
-      reverted = true;
-    }
+      })
+    ).to.revertedWith("");
 
     const factoryAddress = await vPairInstance.factory();
 
-    expect(reverted).to.be.true;
     expect(factoryAddress).to.be.not.equal(vPairFactoryInstance2.address);
   });
 
@@ -162,20 +159,16 @@ contract("vPair", (accounts) => {
   it("Should not set fee if not admin", async () => {
     const feeChange = 1000;
     const vFeeChange = 2000;
-    let reverted = false;
 
-    try {
-      await vPairInstance.setFee(feeChange, vFeeChange, {
+    await expect(
+      vPairInstance.vPairInstance.setFee(feeChange, vFeeChange, {
         from: accounts[1],
-      });
-    } catch (err) {
-      reverted = true;
-    }
+      })
+    ).to.revertedWith("");
 
     const fee = await vPairInstance.fee();
     const vFee = await vPairInstance.vFee();
 
-    expect(reverted).to.be.true;
     expect(fee.toNumber()).to.be.not.equal(feeChange);
     expect(vFee.toNumber()).to.be.not.equal(vFeeChange);
   });
@@ -194,51 +187,101 @@ contract("vPair", (accounts) => {
 
   it("Should not set max reserve threshold if not admin", async () => {
     const thresholdChange = 2000;
-    let reverted = false;
 
-    try {
-      await vPairInstance.setMaxReserveThreshold(feeChange, vFeeChange, {
+    await expect(
+      vPairInstance.setMaxReserveThreshold(feeChange, vFeeChange, {
         from: accounts[1],
-      });
-    } catch (err) {
-      reverted = true;
-    }
-
-    expect(reverted).to.be.true;
+      })
+    ).to.revertedWith("");
   });
-
-    // Main functionality
 
   it("Should mint", async () => {
     await vPairInstance.mint(wallet);
     const liquidity = await vPairInstance.balanceOf(wallet);
-    
+
     let liquidityCalculated = toBn(18, 100);
     liquidityCalculated = liquidityCalculated.mul(toBn(18, 300));
     liquidityCalculated = sqrt(liquidityCalculated);
     liquidityCalculated = liquidityCalculated.sub(toBn(0, 10000));
-    
+
     expect(liquidity.toString()).to.be.equal(liquidityCalculated.toString());
   });
 
   it("Should not mint if liquidity is not greater than 0 after deducting reserve ratio from liquidity", async () => {
     await vPairInstance.mint(wallet);
     const liquidity = await vPairInstance.balanceOf(wallet);
-    
+
     let liquidityCalculated = toBn(18, 100);
     liquidityCalculated = liquidityCalculated.mul(toBn(18, 300));
     liquidityCalculated = sqrt(liquidityCalculated);
     liquidityCalculated = liquidityCalculated.sub(toBn(0, 10000));
-    
+
     expect(liquidity.toString()).to.be.equal(liquidityCalculated.toString());
   });
 
-  // it("Should burn", async () => {});
+  it("Should burn", async () => {
+    await vPairInstance.mint(wallet);
+    const liquidityWallet = await vPairInstance.balanceOf(wallet);
 
-  // //   check require in burn
-  // it("Should not burn if", async () => {});
+    await vPairInstance.transfer(vPairInstance.address, liquidityWallet);
+    const liquidity = await vPairInstance.balanceOf(vPairInstance.address);
+    const totalSupply = await vPairInstance.totalSupply();
 
-  // it("Should swap reserves", async () => {});
+    const aBalancePoolBefore = await tokenA.balanceOf(vPairInstance.address);
+    let aBalancePoolShouldLeft = aBalancePoolBefore.mul(liquidity);
+    aBalancePoolShouldLeft = aBalancePoolShouldLeft.div(totalSupply);
+    const bBalancePoolBefore = await tokenB.balanceOf(vPairInstance.address);
+    let bBalancePoolShouldLeft = bBalancePoolBefore.mul(liquidity);
+    bBalancePoolShouldLeft = bBalancePoolShouldLeft.div(totalSupply);
+    const aBalanceWalletBefore = await tokenA.balanceOf(wallet);
+    const bBalanceWalletBefore = await tokenB.balanceOf(wallet);
+
+    await vPairInstance.burn(wallet);
+    const aBalancePoolAfter = await tokenA.balanceOf(vPairInstance.address);
+    const bBalancePoolAfter = await tokenB.balanceOf(vPairInstance.address);
+    const aBalanceWalletAfter = await tokenA.balanceOf(wallet);
+    const bBalanceWalletAfter = await tokenB.balanceOf(wallet);
+
+    expect(aBalancePoolAfter.lt(aBalancePoolBefore)).to.be.true;
+    expect(bBalancePoolAfter.lt(bBalancePoolBefore)).to.be.true;
+    expect(aBalanceWalletBefore.lt(aBalanceWalletAfter)).to.be.true;
+    expect(bBalanceWalletBefore.lt(bBalanceWalletAfter)).to.be.true;
+
+    expect(
+      aBalancePoolAfter.add(aBalancePoolShouldLeft).toString()
+    ).to.be.equal(aBalancePoolBefore.toString());
+    expect(
+      bBalancePoolAfter.add(bBalancePoolShouldLeft).toString()
+    ).to.be.equal(bBalancePoolBefore.toString());
+
+    expect(
+      aBalanceWalletAfter.sub(aBalanceWalletBefore).toString()
+    ).to.be.equal(aBalancePoolBefore.sub(aBalancePoolAfter).toString());
+    expect(
+      bBalanceWalletAfter.sub(bBalanceWalletBefore).toString()
+    ).to.be.equal(bBalancePoolBefore.sub(bBalancePoolAfter).toString());
+  });
+
+  it("Should not burn if liquidity was not transferred", async () => {
+    await vPairInstance.mint(wallet);
+
+    await expect(vPairInstance.burn(wallet)).to.revertedWith("ILB.");
+  });
+
+  it("Should not burn if balance of one token is 0", async () => {
+    await vPairInstance.mint(wallet);
+
+    const removal = await tokenA.balanceOf(vPairInstance.address);
+    await tokenA.transfer(wallet, removal);
+
+    await expect(vPairInstance.burn(wallet)).to.revertedWith("ILB.");
+  });
+  // WIP
+
+  // it("Should swap reserves", async () => {
+  //   await vPairInstance.mint(wallet);
+  //   const liquidity = await vPairInstance(wallet);
+  // });
 
   // it("Should not swap reserves if calculate reserve ratio is more than max allowed", async () => {});
 
@@ -246,52 +289,79 @@ contract("vPair", (accounts) => {
 
   // it("Should not swap reserves if ik0 is not whitelisted", async () => {});
 
-  // TODO
   // it("Should calculate reserve ratio", async () => {
-  //   await vPairInstance.setWhitelist([tokenA.address, tokenB.address], {
+  //   await vPairInstance.setWhitelist([tokenA.address], {
   //     from: wallet,
   //   });
 
   //   const res1 = await vPairInstance.calculateReserveRatio();
 
-  //   // Nothing was minted reserve0 is 0
-  //   expect(res1.toNumber()).to.be.equal(0);
-
   //   await vPairInstance.mint(wallet);
+
   //   const res2 = await vPairInstance.calculateReserveRatio();
 
-  //   console.log((await vPairInstance.balanceOf(wallet)).toString());
-  //   console.log(res2.toString());
-  //   console.log((await vPairInstance.reserve0()));
-  //   // expect((await vPairInstance.balanceOf(wallet)).eq(toDecimalUnits(18, 10000))).to.equal(true)
+  //   expect((await vPairInstance.balanceOf(wallet)).eq(toDecimalUnits(18, 10000))).to.equal(true)
   // });
 
-  it("Should swap native", async () => {});
+  it("Should swap native", async () => {
+    await vPairInstance.mint(wallet);
 
-  // it("Should not swap native if address is 0", async () => {
-  //   let reverted = false;
-     
-  //   try{
-  //     await debug(await vPairInstance.swapNative(toBn(18, 20), tokenA.address, accounts[1], []));
-  //   }catch (err){
-  //     console.log((await tokenA.balanceOf(wallet)).toString())
-  //     console.log(err);
-  //     reverted = true;
-  //   }
+    const aBalancePoolBefore = await tokenA.balanceOf(vPairInstance.address);
+    const bBalancePoolBefore = await tokenB.balanceOf(vPairInstance.address);
+    const aBalanceWalletBefore = await tokenA.balanceOf(wallet);
+    const bBalanceWalletBefore = await tokenB.balanceOf(wallet);
 
-  //   expect(reverted).to.be.true;
-  // });
+    await tokenB.transfer(vPairInstance.address, toBn(18, 50));
 
-  // it("Should not swap native if amount in is less than expected", async () => {
-  //   const zeroAddress = "0x0000000000000000000000000000000000000000";
-  //   let reverted = false;
-     
-  //   try{
-  //     await vPairInstance.swapNative(100, tokenA.address, zeroAddress, []);
-  //   }catch (err){
-  //     reverted = true;
-  //   }
+    await vPairInstance.swapNative(toBn(18, 1), tokenA.address, wallet, []);
 
-  //   expect(reverted).to.be.true;
-  // });
+    const aBalancePoolAfter = await tokenA.balanceOf(vPairInstance.address);
+    const bBalancePoolAfter = await tokenB.balanceOf(vPairInstance.address);
+    const aBalanceWalletAfter = await tokenA.balanceOf(wallet);
+    const bBalanceWalletAfter = await tokenB.balanceOf(wallet);
+
+    expect(aBalancePoolBefore.toString()).to.be.equal(
+      aBalancePoolAfter.add(toBn(18, 1)).toString()
+    );
+    expect(bBalancePoolAfter.toString()).to.be.equal(
+      bBalancePoolBefore.add(toBn(18, 50)).toString()
+    );
+    expect(aBalanceWalletAfter.toString()).to.be.equal(
+      aBalanceWalletBefore.add(toBn(18, 1)).toString()
+    );
+    expect(bBalanceWalletBefore.toString()).to.be.equal(
+      bBalanceWalletAfter.add(toBn(18, 50)).toString()
+    );
+  });
+
+  it("Should not swap native if address is 0", async () => {
+    const zeroAddress = "0x0000000000000000000000000000000000000000";
+    await vPairInstance.mint(wallet);
+
+    await tokenB.transfer(vPairInstance.address, toBn(18, 50));
+
+    await expect(
+      vPairInstance.swapNative(toBn(18, 1), tokenA.address, zeroAddress, [])
+    ).to.revertedWith("IT");
+  });
+
+  it("Should not swap native if amount exceeds balance", async () => {
+    await vPairInstance.mint(wallet);
+
+    await tokenB.transfer(vPairInstance.address, toBn(18, 50));
+
+    await expect(
+      vPairInstance.swapNative(toBn(21, 1), tokenA.address, wallet, [])
+    ).to.revertedWith("transfer amount exceeds balance");
+  });
+
+  it("Should not swap native if amount exceeds balance", async () => {
+    await vPairInstance.mint(wallet);
+
+    await tokenB.transfer(vPairInstance.address, toBn(18, 50));
+
+    await expect(
+      vPairInstance.swapNative(toBn(19, 6), tokenA.address, wallet, [])
+    ).to.revertedWith("IIA");
+  });
 });
