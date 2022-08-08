1f7b902a796ec2b4b86e31f914c263f25a7e9996
Daniel <daniel@budsoft.co.il>
2022-08-08 22:03:00 +0300

    add pragma

--- contracts/interfaces/IvSwapPoolDeployer.sol
+++ contracts/interfaces/IvSwapPoolDeployer.sol
@@ -1,3 +1,6 @@
+pragma solidity ^0.8.0;
+
+
 /// @title An interface for a contract that is capable of deploying Uniswap V3 Pools
 /// @notice A contract that constructs a pool must implement this to pass arguments to the pool
 /// @dev This is used to avoid having constructor arguments in the pool contract, which results in the init code hash
