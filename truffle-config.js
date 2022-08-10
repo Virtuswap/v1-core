770fc810cde9e70e1ff0de564f65e2911c120e55
Daniel <daniel@budsoft.co.il>
2022-08-10 16:02:21 +0300

    deterministic bytecode

--- truffle-config.js
+++ truffle-config.js
@@ -17,6 +17,12 @@ module.exports = {
           enabled: true,
           runs: 800,
         },
+        metadata: {
+          // do not include the metadata hash, since this is machine dependent
+          // and we want all generated code to be deterministic
+          // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+          bytecodeHash: 'none',
+        }
       },
     },
   },
