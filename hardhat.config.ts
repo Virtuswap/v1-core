b0141edb830f5844d1bcf4bf0d27a199d26dd1c8
Daniel <daniel@budsoft.co.il>
2022-08-15 17:13:49 +0300

    add deterministic bytecode

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,13 +10,19 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 800,
       },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        bytecodeHash: "none",
+      },
     },
   },
   networks: {
-    quicknodeTestWS: {
-      url: extractStringEnvVar("QUICKNODE_WS")
-    }
-  }
+    // quicknodeTestWS: {
+    //   url: extractStringEnvVar("QUICKNODE_WS")
+    // }
+  },
 };
 
 export default config;
