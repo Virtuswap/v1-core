c4c923c68cfdf2e5634eb0fc61996dc7606e63d4
danielj86 <daniel@budsoft.co.il>
2023-01-03 12:58:27 +0200

    add compile task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-contract-sizer";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import './tasks/compile';
+
+import 'hardhat-contract-sizer';
 
 const config: HardhatUserConfig = {
-  contractSizer: {
-    alphaSort: true,
-    runOnCompile: true,
-    disambiguatePaths: false,
-  },
-  solidity: {
-    version: "0.8.2",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 800,
-      },
-
-      metadata: {
-        // do not include the metadata hash, since this is machine dependent
-        // and we want all generated code to be deterministic
-        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
-        bytecodeHash: "none",
-      },
+    contractSizer: {
+        alphaSort: true,
+        runOnCompile: true,
+        disambiguatePaths: false,
     },
-  },
+    solidity: {
+        version: '0.8.2',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 800,
+            },
+
+            metadata: {
+                // do not include the metadata hash, since this is machine dependent
+                // and we want all generated code to be deterministic
+                // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+                bytecodeHash: 'none',
+            },
+        },
+    },
 };
 
 export default config;
