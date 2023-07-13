5823d881b796f624d90aa9a08775abadefbee9f9
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2023-07-13 18:34:03 +0300

    Update runs value

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 799,
+                runs: 216,
             },
 
             metadata: {
